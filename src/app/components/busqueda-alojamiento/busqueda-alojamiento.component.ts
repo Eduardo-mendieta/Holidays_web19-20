9d46dcda4e2da274ab4e3e9cdd40bca19a22450f
Edward_O <chrisseduu13mc@gmail.com>
2020-09-20 14:28:01 -0500

    Creación del hml del buscador de alojamientos

---
+++ src/app/components/busqueda-alojamiento/busqueda-alojamiento.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-busqueda-alojamiento',
+  templateUrl: './busqueda-alojamiento.component.html',
+  styleUrls: ['./busqueda-alojamiento.component.css']
+})
+export class BusquedaAlojamientoComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
